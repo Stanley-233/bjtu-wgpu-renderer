150848ad8399c73b57f6ade260bc19d6ea3e7d51
Stanley <acc_stanley@foxmail.com>
2026-03-03 19:38:20 +0800

    feat: device and adapter initialization

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,63 @@
 #include <webgpu/webgpu.hpp>
 
 #include <iostream>
+#include <vector>
 
-int main (int, char**) {
-	// We create a descriptor
+#include "utils.h"
+
+int main(int, char **) {
+	// Descriptor serves as a configuration for the instance.
+	// It is optional, but may be used to specify additional features or limits.
 	WGPUInstanceDescriptor desc = {};
 	desc.nextInChain = nullptr;
 
-	// We create the instance using this descriptor
-#ifdef WEBGPU_BACKEND_EMSCRIPTEN
+	// Emscripten requires nullptr as default descriptor
+#ifdef __EMSCRIPTEN__
 	WGPUInstance instance = wgpuCreateInstance(nullptr);
-#else //  WEBGPU_BACKEND_EMSCRIPTEN
+#else
 	WGPUInstance instance = wgpuCreateInstance(&desc);
-#endif //  WEBGPU_BACKEND_EMSCRIPTEN
+#endif
 
-	// We can check whether there is actually an instance created
 	if (!instance) {
 		std::cerr << "Could not initialize WebGPU!" << std::endl;
 		return 1;
 	}
-
-	// Display the object (WGPUInstance is a simple pointer, it may be
-	// copied around without worrying about its size).
 	std::cout << "WGPU instance: " << instance << std::endl;
 
-	// We clean up the WebGPU instance
+	std::cout << "Requesting adapter..." << std::endl;
+	WGPURequestAdapterOptions adapterOpts = {};
+	adapterOpts.nextInChain = nullptr;
+	WGPUAdapter adapter = requestAdapterSync(instance, &adapterOpts);
+	std::cout << "Got adapter: " << adapter << std::endl;
+	inspectAdapter(adapter);
 	wgpuInstanceRelease(instance);
+
+	std::cout << "Requesting device..." << std::endl;
+	WGPUDeviceDescriptor deviceDesc = {};
+	deviceDesc.nextInChain = nullptr;
+	deviceDesc.label = "My Device"; // anything works here, that's your call
+	deviceDesc.requiredFeatureCount = 0; // we do not require any specific feature
+	deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+	deviceDesc.defaultQueue.nextInChain = nullptr;
+	deviceDesc.defaultQueue.label = "The default queue";
+	// A function that is invoked whenever the device stops being available.
+	deviceDesc.deviceLostCallback = [](WGPUDeviceLostReason reason, char const* message, void* /* pUserData */) {
+		std::cout << "Device lost: reason " << reason;
+		if (message) std::cout << " (" << message << ")";
+		std::cout << std::endl;
+	};
+	WGPUDevice device = requestDeviceSync(adapter, &deviceDesc);
+	std::cout << "Got device: " << device << std::endl;
+	auto onDeviceError = [](WGPUErrorType type, char const* message, void* /* pUserData */) {
+		std::cout << "Uncaptured device error: type " << type;
+		if (message) std::cout << " (" << message << ")";
+		std::cout << std::endl;
+	};
+	wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
+	wgpuAdapterRelease(adapter);
+	inspectDevice(device);
+
+	wgpuDeviceRelease(device);
 
 	return 0;
 }
