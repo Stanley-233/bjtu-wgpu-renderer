403de81cbea331e97dde9b61b653ed8a4bec283b
54shitaimzf <mzfshitiancai@126.com>
2026-04-07 00:34:08 +0800

    feat(scene): 实现PerspectiveCemera的view与Projection
    完成了view矩阵与投影矩阵的计算部分

--- src/scene/camera/PerspectiveCamera.cpp
+++ src/scene/camera/PerspectiveCamera.cpp
@@ -8,11 +8,25 @@ void PerspectiveCamera::SetPerspective(const float fovYRadians, const float near
 
 glm::mat4 PerspectiveCamera::View() const {
     // TODO: 根据相机位置/目标/up 计算并返回透视相机 View 矩阵
-    return glm::mat4(1.0f);
+    const glm::vec3 f = glm::normalize(m_target - m_position);
+    const glm::vec3 s = glm::normalize(glm::cross(f, m_up));
+    const glm::vec3 u = glm::cross(s, f); 
+    return glm::mat4(
+        glm::vec4(s,0),
+        glm::vec4(u,0),
+        glm::vec4(-f,0),
+        glm::vec4(-glm::dot(s, m_position), -glm::dot(u, m_position), glm::dot(f, m_position), 1.0f)
+    );
 }
 
 glm::mat4 PerspectiveCamera::Projection(const float aspect) const {
     // TODO: 根据 fov、near、far、aspect 计算透视投影矩阵
-    (void)aspect;
-    return glm::mat4(1.0f);
+    const float t = 1.0f / std::tan( m_fovYRadians * 0.5f);
+    const float temp = m_farPlane / (m_nearPlane - m_farPlane);
+    return glm::mat4(
+        glm::vec4(t / aspect, 0, 0, 0),
+        glm::vec4(0, -t, 0, 0),
+        glm::vec4(0, 0, temp, -m_nearPlane * temp),
+        glm::vec4(0, 0, 1.0f, 0)
+    );
 }
